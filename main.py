875f170b72b57172ab1dd3b85c7e0fee7cb64b72
Khoa <khoanguyen12062005@gmail.com>
2026-03-14 23:02:29 +0700

    m

--- main.py
+++ main.py
@@ -208,7 +208,6 @@ def main():
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
     np.random.seed(args.seed)
-    random.seed(args.seed)
 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
